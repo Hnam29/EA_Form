133bbade639921a0fe16fe42091f42b59a1cddd1
Hector29 <114966508+Hnam29@users.noreply.github.com>
2024-11-01 16:01:54 +0700

    Update form_ggs.py

--- form_ggs.py
+++ form_ggs.py
@@ -218,7 +218,7 @@ def form_creation():
         name = st.text_input('Họ và tên của Anh/Chị: ')
         company = st.text_input('Tên doanh nghiệp của Anh/Chị: ')
         roles = ["C-level", "M-level", "E-level"]  # Define your roles
-        role = st.selectbox('Chức vụ của Anh/Chị:', options=roles, index=roles[0])
+        role = st.selectbox('Chức vụ của Anh/Chị:', options=roles, index=0)
         phoneNo = st.text_input('Số điện thoại của Anh/Chị: ')
         email = st.text_input('Địa chỉ email của Anh/Chị: ')
         sentiment = st.slider("Rate your experience:", 1, 5, 1, format="%d ⭐")
